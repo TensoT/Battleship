d8a618ed56703ff89c876610ab4c5103a979bf03
Ubuntu_16.04 <nassiem.hadeil@hsbi.de>
2025-11-19 16:26:23 +0100

    Update 1

--- main.C
+++ main.C
@@ -1,33 +1,29 @@
 //============================================================================
 // Name        : INF3_Prak.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C, Ansi-style
 //============================================================================
 
-#include <string>
+//#include <string>
 #include <iostream>
-
-#include <cstdio>      /* printf, NULL */
+//#include <cstdio>      /* printf, NULL */
 #include <cstdlib>     /* srand, rand */
 #include <ctime>       /* time */
-
-#include <unistd.h>
-
-#include "SHA256.H"
-#include "TASK1.H"
-#include "TASK2.H"
+//#include <unistd.h>
+//#include "SHA256.H"
 #include "TASK3.H"
-#include "TASK4.H"
-#include "TASK5.H"
-#include "TASK6.H"
+//#include "TASK3.C"
+
 
 using namespace std;
 
 
 int main(){
-	srand(time(nullptr));
+	srand(time(NULL));
+
+	TASK3::demoTask3();
 
     return 0;
 }
@@ -35,4 +31,4 @@ int main(){
 
 
 
-
+//Änderung
